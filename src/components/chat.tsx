856b2fb9221d8193cd47c3d311c1137a3bc52e99
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2025-04-09 22:29:59 -0700

    chat coming together

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,3 +1,3 @@
 export default function Chat() {
-  return <div>chat</div>;
+  return <div className="h-full pt-4">chat</div>;
 }
--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,3 +1,3 @@
 export default function Preview() {
-  return <div>preview</div>;
+  return <div className="h-full pt-4">preview</div>;
 }
