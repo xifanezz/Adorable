16c9f64054a57a509f0d942a4ab80ed37bbecb4b
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2025-04-09 22:28:24 -0700

    chat and preview ui

--- src/app/app/[id]/page.tsx
+++ src/app/app/[id]/page.tsx
@@ -1,11 +1,14 @@
+import Chat from "../../../components/chat";
+import Preview from "../../../components/preview";
+
 export default function IdPage() {
   return (
     <div className="flex min-h-screen">
       <div className="w-1/4 border-r p-4">
-        {/* Left column content */}
+        <Chat />
       </div>
       <div className="w-3/4 p-4">
-        {/* Right column content */}
+        <Preview />
       </div>
     </div>
   );
