553c4c6256c4638461f61e9a5155696239c317cb
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2025-04-09 22:25:27 -0700

    simple chat started

---
+++ src/app/app/[id]/page.tsx
@@ -0,0 +1,12 @@
+export default function IdPage() {
+  return (
+    <div className="flex min-h-screen">
+      <div className="w-1/4 border-r p-4">
+        {/* Left column content */}
+      </div>
+      <div className="w-3/4 p-4">
+        {/* Right column content */}
+      </div>
+    </div>
+  );
+}
