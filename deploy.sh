9005d85bc4834bc6436a3ed15dc623be185a4323
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2025-04-18 15:15:44 -0700

    adorable.dev deployable and deployed

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+npm run build
+
+cp -r public .next/standalone/public
+cp -r .next/static .next/standalone/.next/static
+cp package-lock.json .next/standalone/package-lock.json
+cp .env.production .next/standalone/.env.production
+cp .env .next/standalone/.env
+
+cd .next/standalone
+npx freestyle deploy --web server.js --domain adorable.dev
