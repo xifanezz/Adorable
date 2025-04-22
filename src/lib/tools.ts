17ad3e14752b8df13c8f56cad5451552199fb9f9
Jacob Zwang <59858341+JacobZwang@users.noreply.github.com>
2025-04-21 20:28:34 -0700

    mcp cleanup

--- src/lib/tools.ts
+++ src/lib/tools.ts
@@ -1,32 +1,7 @@
 // import { type ToolSet } from "ai";
 
-import { experimental_createMCPClient as createMCPClient, JSONRPCMessage, MCPTransport } from "ai";
-// import { Client } from "@modelcontextprotocol/sdk/client/index.js";
+import { experimental_createMCPClient as createMCPClient } from "ai";
 import { StreamableHTTPClientTransport } from "@modelcontextprotocol/sdk/client/streamableHttp.js";
-
-// class SSETransport implements MCPTransport {
-//   client: Client;
-//   transport: StreamableHTTPClientTransport;
-
-//   constructor(client: Client) {
-//     this.client = client;
-//   }
-
-//   close(): Promise<void> {
-//     return this.client.close()
-
-
-//   }
-
-//   send(message: JSONRPCMessage): Promise<void> {
-//     console.log(message);
-//     // this.client.fallbackRequestHandler()
-//   }
-//   start(): Promise<void> {
-//     return this.client.connect()
-//   }
-// }
-
 
 export const ADORABLE_TOOLS = async ({ mcpUrl }: { mcpUrl: string }) => {
   const devServerMcp = await createMCPClient({
