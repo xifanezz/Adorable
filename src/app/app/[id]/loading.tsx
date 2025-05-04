b6a3b3b3082f9d29068f739db379b02787293e0a
Jacob Zwang <59858341+JacobZwang@users.noreply.github.com>
2025-05-04 12:12:25 -0700

    add loading page for existing apps

---
+++ src/app/app/[id]/loading.tsx
@@ -0,0 +1,15 @@
+import { unstable_ViewTransition as ViewTransition } from "react";
+import "@/components/loader.css";
+
+export default function Loading() {
+  return (
+    <ViewTransition>
+      <div className="flex justify-center items-center h-screen">
+        <div>
+          <div className="text-center">Loading App</div>
+          <div className="loader"></div>
+        </div>
+      </div>
+    </ViewTransition>
+  );
+}
