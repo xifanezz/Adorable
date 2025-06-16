d818500c41ecdbf9b837a8e810830f2fb4896688
Jacob Zwang <59858341+JacobZwang@users.noreply.github.com>
2025-06-15 18:36:30 -0700

    gate firecracker behind env variable

--- src/lib/freestyle.ts
+++ src/lib/freestyle.ts
@@ -4,8 +4,10 @@ export const freestyle = new FreestyleSandboxes({
   apiKey: process.env.FREESTYLE_API_KEY!,
   baseUrl: process.env.FREESTYLE_API_URL ?? "https://api.freestyle.sh",
   headers: {
-    "Freestyle-Firecracker-Dev-Servers": "true",
-    "Freestyle-Firecracker-Build-Servers": "true",
+    "Freestyle-Firecracker-Dev-Servers":
+      process.env.FREESTYLE_FIRECRACKER === "true" ? "true" : "false",
+    "Freestyle-Firecracker-Build-Servers":
+      process.env.FREESTYLE_FIRECRACKER === "true" ? "true" : "false",
     Authorization: `Bearer ${process.env.FREESTYLE_API_KEY}`,
   },
 });
